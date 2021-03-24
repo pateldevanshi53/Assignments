a8b0ec4d0fc105c9ee8c6e1b207295253575205f
pateldevanshi53 <80935447+pateldevanshi53@users.noreply.github.com>
2021-03-23 17:07:51 -0700

    Update T1_3.java

--- Task1/T1_3.java
+++ Task1/T1_3.java
@@ -1,10 +1,10 @@
-package task1;
 
 //Write a program to complete the task given below:
 /* Ask the user to enter any 2 numbers in between 1-10 and add both of them to another variable call z
  * Use z for adding 30 into it and print the final result by using variable results
  */
 
+package task1;
 
 import java.util.Scanner;
 
